f690bcd8aa4ff3790fa24f9e94e6be50f09125b8
jshinv <jshinv@gmail.com>
2020-04-21 18:32:48 +0900

    add models for table

--- face/face/settings.py
+++ face/face/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'face.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'people',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': 3306
+       
     }
 }
 
